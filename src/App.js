aa295acb27085c6a8fedd354f63dd8bae7c469fc
Abdulaziz Almohammadi <AbdulazizAlmohammadi@users.noreply.github.com>
2021-06-10 13:17:58 +0300

    Abdulaziz Almohammadi

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
+import Form from "./Components/Form";
+import "./App.css";
+import Home from "./Components/Home";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      picture: "",
+      token: null,
+    };
+  }
+  componentDidMount() {
+    if (localStorage.getItem("token")) {
+      this.setState({
+        name: localStorage.getItem("name"),
+        email: localStorage.getItem("email"),
+        picture: localStorage.getItem("picture"),
+        token: localStorage.getItem("token"),
+      });
+      console.log(this.state.token);
+    }
+  }
+
+  componentDidUpdate = () => {
+    if (this.state.token) {
+      localStorage.setItem("name", this.state.name);
+      localStorage.setItem("email", this.state.email);
+      localStorage.setItem("picture", this.state.picture);
+      localStorage.setItem("token", this.state.token);
+    }
+  };
+
+  responseFacebook = (response) => {
+    console.log(response);
+
+    this.setState({
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url,
+      token: response.accessToken,
+    });
+  };
+
+  render() {
+    return (
+      <center>
+        {this.state.token ? (
+          <Home />
+        ) : (
+          <div>
+            <h1>Log In</h1>
+            <FacebookLogin
+              appId="3633847266720370"
+              autoLoad={false}
+              fields="name,email,picture"
+              callback={this.responseFacebook}
+            />
+          </div>
+        )}
+      </center>
+    );
+  }
+}

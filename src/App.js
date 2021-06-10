9fd61b6f16e63f6f3f659805632b2d4329832132
ThamerMashni <thamer.mashni@tuwaiq.edu.sa>
2021-06-10 18:46:04 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
+import Form from "./Components/Form";
+import "./App.css";
+import Home from "./Components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      picture: "",
+      token: null,
+    };
+  }
+  componentDidMount() {
+    if (localStorage.getItem("token")) {
+      this.setState({
+        name: localStorage.getItem("name"),
+        email: localStorage.getItem("email"),
+        picture: localStorage.getItem("picture"),
+        token: localStorage.getItem("token"),
+      });
+      console.log(this.state.token);
+    }
+  }
+
+  componentDidUpdate = () => {
+    if (this.state.token) {
+      localStorage.setItem("name", this.state.name);
+      localStorage.setItem("email", this.state.email);
+      localStorage.setItem("picture", this.state.picture);
+      localStorage.setItem("token", this.state.token);
+    }
+  };
+
+  responseFacebook = (response) => {
+    console.log(response);
+
+    this.setState({
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url,
+      token: response.accessToken,
+    });
+  };
+
+  render() {
+    return (
+      <center>
+        {this.state.token ? (
+          <Home />
+        ) : (
+          <div>
+            <h1>Log In</h1>
+            <FacebookLogin
+              appId="1492015204466948"
+              autoLoad={false}
+              fields="name,email,picture"
+              callback={this.responseFacebook}
+            />
+          </div>
+        )}
+      </center>
+    );
+  }
 }
-
-export default App;
